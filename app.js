575ee05d1cbd5dcb4240b62aa802b04c53c18de7
MiG <yanrancanfei@163.com>
2022-07-11 14:24:22 +0800

    add

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const Koa = require('koa')
 const config = require('config')
 const bodyParser = require('koa-bodyparser')
 const responseTime = require('koa-response-time')
+const jwt = require('koa-jwt')
+const cors = require('@koa/cors')
+
 const app = new Koa()
 
 const logger = require('./app/utils/logger')
@@ -22,10 +25,19 @@ const model = require('./app/models/index')
 const logError = require('./app/listeners/logError')
 const routers = require('./app/routers')
 const port = 3000
+const TOKEN_KEY = ''
 
 logger.info(`[env: ${process.env.NODE_ENV}] retail-pm service run on ${port}`)
 app.context.config = config
 app
+  .use(
+    cors({
+      origin: '*',
+      credentials: true,
+      allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'] // 设置所允许的HTTP请求方法
+      // allowHeaders: ['Content-Type', 'Authorization', 'Accept'] // 设置服务器支持的所有头信息字段
+    })
+  )
   .on('error', logError())
   .use(attachCtxLogger(logger))
   .use(responseTime())
@@ -36,6 +48,7 @@ app
   .use(handle4xxError())
   .use(handle401Error())
   .use(attachCtxModel(model))
+  .use(jwt({ secret: TOKEN_KEY }).unless({ path: [/^\/login/, /^\/register/, /^\/uploadIcon/] })) // usless排除进行jwt校验的路由
   .use(attachCtxRedis(redis))
   // the application only supports json payload
   .use(
--- app/utils/__tests__/index.spec.js
+++ app/utils/__tests__/index.spec.js
@@ -1,7 +1,5 @@
 'use strict'
 
-const { compareArr } = require('../index')
-
 function sum(a, b) {
   return a + b
 }
@@ -9,9 +7,5 @@ function sum(a, b) {
 describe('test util', () => {
   it('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).toBe(3)
-  })
-  it('test compareArr', () => {
-    const diff = compareArr([1, 2, 3, 4], [1, 2, 3, 4])
-    expect(diff).toEqual(true)
   })
 })
