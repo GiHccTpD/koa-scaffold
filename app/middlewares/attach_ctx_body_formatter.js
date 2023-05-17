ccaae15335ffb412375ee0553094753d2714657c
jinshengxu <jinshengxu@inspur.com>
2023-05-17 15:39:10 +0800

    feat: 提交

--- app/middlewares/attach_ctx_body_formatter.js
+++ app/middlewares/attach_ctx_body_formatter.js
@@ -1,30 +1,30 @@
-'use strict';
+'use strict'
 
-function routerResponse() {
+function routerResponse () {
   return async function (ctx, next) {
     ctx.success = function (data) {
-      ctx.res.statusCode = 200;
+      ctx.res.statusCode = 200
       ctx.body = {
         code: 0,
         msg: 'success',
         data: data || {}
-      };
-    };
+      }
+    }
 
     ctx.fail = function ({ code, msg, data }) {
       if (!msg) {
-        msg = '';
+        msg = ''
       }
 
       ctx.body = {
         code: code,
         msg,
         data: data || {}
-      };
-    };
+      }
+    }
 
-    await next();
-  };
+    await next()
+  }
 }
 
-module.exports = routerResponse;
+module.exports = routerResponse
